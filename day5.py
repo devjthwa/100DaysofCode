c748d54fd962e3374b1a718885ea0ace50b00c92
Dev Jethwa <90303858+devjthwa@users.noreply.github.com>
2022-12-26 18:57:18 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,25 @@
+# '''Hey Dev, Don't remove this line
+# abc
+# cdes'''
+# print("I am a \"good boy\" \nand this viewer is also a good boy") #printing this line
+# print("Hello WOrld",8,6,7,sep='~', end="009\n") #sep separates values by chosen 
+# print('Dev')
+
+#day6 variables and data types
+'''a = 1 #a is a variable that stores a value 1
+a1 = 96
+b2 = 10.5
+b = 'Dev' 
+print(a)
+print(b)
+print(a+a1)
+print(type(a))
+print(type(b))
+print(type(b2))
+list1 = [7, 19.5, [-4,5],['Dev','Yatin']] # data type :list
+tuple1 = (("lion","Tiger",'Crow')) # data type :tuple
+print(list, tuple1) 
+dict1 = {'name': 'Dev', 'age' : 20, 'canvote':True} # data type : dictionary
+print(dict1)
+a3 = complex(1,5) # data type :complex
+print(a3) '''
---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello World")
+print(5)
+print(8+7) #prints 15
+print(8*7)
+#typing cast 9th day
+a = '5'
+b = '10' 
+print(int(a) + int(b))
